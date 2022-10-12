cec70243cf8e8b9fdfc0be22617a8cde92d80251
Carlos-vargs <cvargaslopez769@gmail.com>
2022-10-12 12:01:02 -0600

    :alien: update social network data
    split social network data

---
+++ src/SocialNetworkData.js
@@ -0,0 +1,31 @@
+import GithubIcon from "./resources/icons/GithubIcon";
+import TwitterIcon from "./resources/icons/TwitterIcon";
+import FacebookIcon from "./resources/icons/FacebookIcon";
+import InstagramIcon from "./resources/icons/InstagramIcon";
+
+const data = [
+  {
+    socialNetwork: "Facebook",
+    icon: <FacebookIcon />,
+    url: "https://www.facebook.com/nccarlosvargas",
+    pl: "6px",
+  },
+  {
+    socialNetwork: "Instagram",
+    icon: <InstagramIcon />,
+    url: "https://www.instagram.com/nc_cvargas",
+  },
+  {
+    socialNetwork: "Github",
+    icon: <GithubIcon />,
+    url: "https://github.com/Carlos-vargs",
+  },
+  {
+    socialNetwork: "Twitter",
+    icon: <TwitterIcon />,
+    url: "https://twitter.com/nc_cvargas",
+    pt: "2px",
+  },
+];
+
+export default data;
